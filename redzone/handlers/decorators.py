bcf0b149db9779a1e568312a36f9b1aa652a07cb
dhays-redzone <dhays@redzone.co>
2025-10-02 11:47:02 -0700

    renamed to handler and moved into handlers dir

--- redzone/handlers/decorators.py
+++ redzone/handlers/decorators.py
@@ -3,7 +3,7 @@ from functools import wraps
 from typing import Callable
 
 
-def synchronous(func: Callable) -> Callable:
+def handler(func: Callable) -> Callable:
     @wraps(func)
     def wrapper(*args, **kwargs):
         return asyncio.run(func(*args, **kwargs))
