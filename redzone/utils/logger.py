cfe886bfe0cb1048a5bf0bb37b785248c1b34793
Evan Furbeyre <efurbeyre@redzone.co>
2025-10-29 16:05:44 -0700

    truncate logs to 1000

--- redzone/utils/logger.py
+++ redzone/utils/logger.py
@@ -1,10 +1,20 @@
+import logging
+
 from microgue.loggers.logger import Logger as _Logger
 
 from ..settings import PROJECT_NAME
 
+MAX_LOG_LENGTH = 1000
+
 
 class Logger(_Logger):
-    pass
+    __instance = None
+
+    def log(self, message, priority=None, level=logging.DEBUG):
+        message = str(message)
+        if len(message) > MAX_LOG_LENGTH:
+            message = message[:MAX_LOG_LENGTH] + "... (truncated)"
+        super().log(message, priority, level)
 
 
 logger = Logger(PROJECT_NAME)
