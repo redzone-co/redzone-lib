7c5ccfbf35b158e813d0ddda08a974f01067d0ea
Alex Cheung <acheung@redzone.co>
2026-06-12 15:50:15 -0700

    try catch on db connection

--- redzone/utils/database.py
+++ redzone/utils/database.py
@@ -33,8 +33,11 @@ class Database:
     @asynccontextmanager
     async def connection(self):
         # provide context manager to start and close database session
-        yield self.start_session()
-        await self.close_session()
+        session = self.start_session()
+        try:
+            yield session
+        finally:
+            await self.close_session()
 
     def start_session(self) -> AsyncSession:
         # start a new database session
